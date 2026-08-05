51f5be2a719acf2e602e2e5e1bdc84657d119553
25b01a1242-crypto <25b01a1242@svecw.edu.in>
2026-08-05 16:24:03 +0530

    Update student.java

--- student.java
+++ student.java
@@ -1,62 +1,79 @@
 import java.util.Scanner;
-class student{
-public static void main(String args[]){
-int s1,s2,s3,s4,s5,s6,s7,s8,s9;
-double gp;
-double cr;
+class Student
+{
+int s1,s2,s3,s4,s5,s6,s7,s8;
+public static void main(String arg[]){
+int gp;
+double SGPA;
+double prev_sgpa;
+double credits=0;
 double total=0;
-double sgpa;
-int i;
+
+double sgpa=0;
 Scanner sc=new Scanner(System.in);
-for(i=1;i<=9;i++){
-System.out.println("Enter the Student"+i+"Marks");
-int marks=sc.nextInt();
-if(marks>=90){
-System.out.println("S");
-gp=10.0;
-}
-else if(marks>=80){
-System.out.println("A");
-gp=9.0;
-}
-else if(marks>=70){
-System.out.println("B");
-gp=8.0;
-}
-else if(marks>=60){
-System.out.println("C");
-gp=7.0;
-}
-else if(marks>=50){
-System.out.println("D");
-gp=6.0;
-}
-else if(marks>=40){
-System.out.println("E");
-gp=5.0;
-}
-else {
-System.out.println("Fail");
-gp=0.0;
-}
+System.out.println("Enter number of students:");
+int Student=sc.nextInt();
+System.out.println("Enter number of subjects:");
+int sub=sc.nextInt();
+int i=1;
+while(i<=Student)
+{
+	total = 0;
+        sgpa=0;
+	System.out.println("For Student"+i+":");
+	for(int j=1;j<=sub;j++){
+	System.out.println("Enter subject"+j+"marks");
+	int marks=sc.nextInt();
+	System.out.println("Subject"+j+"Grade :");
+	if(marks>=90 && marks<=100){
+	System.out.println("S");
+	gp=10;
+	}
+	else if(marks>=80){
+	System.out.println("A");
+	gp=9;
 
-if(i==1 || i==2 || i==3 || i==4 || i==5)
-cr=3.0;
+	}
+	else if(marks>=70){
+	System.out.println("B");
+	gp=8;
+	}
+	else if(marks>=60){
+	System.out.println("C");
+	gp=7;
+	}
+	else if(marks>=50){
+	System.out.println("D");
+	gp=6;
+	}
+	else if(marks>=40){
+	System.out.println("E");
+	gp=5;
+	}
+	else {
+	System.out.println("Fail");
+	gp=0;
+	}
 
-else if(i==6 || i==7)
-cr=1.5;
+System.out.println("Enter credits for subject"+i);
+credits=sc.nextDouble();
 
-else if(i==8)
-cr=1.0;
+sgpa=(sgpa+(credits*gp));
 
-else 
-cr=0.5;
+		total=total+credits;
 
-total=total+(cr*gp);
+	
+
 }
-sgpa=total/19.5;
-System.out.println("SGPA:" + sgpa);
+	SGPA=sgpa/total;
+	System.out.println("SGPA :"+SGPA);
+	System.out.println("Enter previous SGPA:");
+	prev_sgpa=sc.nextDouble();
+	double CGPA=((prev_sgpa+SGPA)*(0.5));
+	System.out.println("CGPA:"+CGPA);
+
+i++;
+	}
+
 }
 }
-
-
